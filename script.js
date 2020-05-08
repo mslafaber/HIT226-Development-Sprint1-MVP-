bc22ff464f2d34c915e2bcf709dc5e7373ed1f71
kanishchhabra <s316921@students.cdu.edu.au>
2020-05-09 04:33:14 +0930

    Minor updates in payment options

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
-$(document).ready(function () {
+$(document).ready(function () {   
     $("#pasta, #sides, #drinks, #meals, #payment").css("display", "none");
     $("#pizza_button").click(function () { 
         $("#pizza").show();
         $("#pasta, #sides, #drinks, #meals, #payment").hide();
         $("#pasta_button, #sides_button, #drinks_button, #meals_button").css({"border": "none", "background-color":"#bf0603"});
-        $("#pizza_button").css({"border": "black solid", "background-color":"red"});
-
+        $("#pizza_button").css({"background-color":"red"});
 
     });
     $("#pasta_button").click(function () { 
         $("#pasta").show();
         $("#pizza, #sides, #drinks, #meals, #payment").hide();
         $("#pizza_button, #sides_button, #drinks_button, #meals_button").css({"border": "none", "background-color":"#bf0603"});
-        $("#pasta_button").css({"border": "black solid", "background-color":"red"});
+        $("#pasta_button").css({"background-color":"red"});
     });
     $("#sides_button").click(function () { 
         $("#sides").show();
         $("#pizza, #pasta, #drinks, #meals, #payment").hide();
         $("#pasta_button, #pizza_button, #drinks_button, #meals_button").css({"border": "none", "background-color":"#bf0603"});
-        $("#sides_button").css({"border": "black solid", "background-color":"red"});
+        $("#sides_button").css({"background-color":"red"});
     });
     $("#drinks_button").click(function () { 
         $("#drinks").show();
         $("#pizza, #sides, #pasta, #meals, #payment").hide();
         $("#pasta_button, #sides_button, #pizza_button, #meals_button").css({"border": "none", "background-color":"#bf0603"});
-        $("#drinks_button").css({"border": "black solid", "background-color":"red"});
+        $("#drinks_button").css({"background-color":"red"});
     });
     $("#meals_button").click(function () { 
         $("#meals").show();
         $("#pizza, #sides, #drinks, #pasta, #payment").hide();
         $("#pasta_button, #sides_button, #drinks_button, #pizza_button").css({"border": "none", "background-color":"#bf0603"});
-        $("#meals_button").css({"border": "black solid", "background-color":"red"});
+        $("#meals_button").css({"background-color":"red"});
     });
     $("#order_next_button").click(function() {
         $(".order_time").hide();
         $(".payment_time").show(); 
         $(".order_container").slideUp(175, function () {
-            $("#payment, #payment_form").slideDown(175);
+            $("#payment").slideDown(175);
         });
     });
     $("#payment_back_button").click(function() { 
@@ -46,7 +45,7 @@ $(document).ready(function () {
             $(".order_container").slideDown(175, function(){
                 $("#pizza").slideDown();
                 $("#pasta_button, #sides_button, #drinks_button, #meals_button").css({"border": "none", "background-color":"#bf0603"});
-                $("#pizza_button").css({"border": "black solid", "background-color":"red"});
+                $("#pizza_button").css({"background-color":"red"});
             });
             $("#pasta, #sides, #drinks, #meals").hide();
         });
@@ -69,8 +68,14 @@ $(document).ready(function () {
         $("#credit_card_form").slideDown(175, function () {
             $(".cc_elements").attr('required', true);
         });
+        $("#pay_in_store_form").slideUp(175, function () {
+            $(".pis_elements").attr('required', false);
+        });
     });
     $("#pay_in_store").click(function () {
+        $("#pay_in_store_form").slideDown(175, function () {
+            $(".pis_elements").attr('required', true);
+        });
         $("#credit_card_form").slideUp(175, function () {
             $(".cc_elements").attr('required', false);
         });
