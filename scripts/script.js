cb6f3964a229bf32074e532a4e3c345901cc79a0
kanishchhabra <s316921@students.cdu.edu.au>
2020-05-10 04:55:38 +0930

    Rewamped the content items to make it more user-friendly Added a bit of reponsiveness to the webpage

--- scripts/script.js
+++ scripts/script.js
@@ -34,15 +34,15 @@ $(document).ready(function () {
     $("#order_next_button").click(function() {
         $(".order_time").hide();
         $(".payment_time").show(); 
-        $(".order_container").slideUp(175, function () {
-            $("#payment").slideDown(175);
+        $(".order_container").slideUp(300, function () {
+            $("#payment").slideDown(300);
         });
     });
     $("#payment_back_button").click(function() { 
         $(".order_time").show();
         $(".payment_time").hide(); 
-        $("#payment").slideUp(175, function(){
-            $(".order_container").slideDown(175, function(){
+        $("#payment").slideUp(300, function(){
+            $(".order_container").slideDown(300, function(){
                 $("#pizza").slideDown();
                 $("#pasta_button, #sides_button, #drinks_button, #meals_button").css({"border": "none", "background-color":"#bf0603"});
                 $("#pizza_button").css({"background-color":"red"});
@@ -53,30 +53,30 @@ $(document).ready(function () {
     $("#payment_next_button").click(function() {
         $(".payment_time").hide();
         $(".confirmation_time").show(); 
-        $("#payment").slideUp(175, function () {
-            $("#confirmation").slideDown(175);
+        $("#payment").slideUp(300, function () {
+            $("#confirmation").slideDown(300);
         });
     });
     $("#confirmation_back_button").click(function() {
         $(".payment_time").show();
         $(".confirmation_time").hide(); 
-        $("#confirmation").slideUp(175, function () {
-            $("#payment").slideDown(175);
+        $("#confirmation").slideUp(300, function () {
+            $("#payment").slideDown(300);
         });
     });
     $("#credit_card").click(function () { 
-        $("#credit_card_form").slideDown(175, function () {
+        $("#credit_card_form").slideDown(300, function () {
             $(".cc_elements").attr('required', true);
         });
-        $("#pay_in_store_form").slideUp(175, function () {
+        $("#pay_in_store_form").slideUp(300, function () {
             $(".pis_elements").attr('required', false);
         });
     });
     $("#pay_in_store").click(function () {
-        $("#pay_in_store_form").slideDown(175, function () {
+        $("#pay_in_store_form").slideDown(300, function () {
             $(".pis_elements").attr('required', true);
         });
-        $("#credit_card_form").slideUp(175, function () {
+        $("#credit_card_form").slideUp(300, function () {
             $(".cc_elements").attr('required', false);
         });
     });
