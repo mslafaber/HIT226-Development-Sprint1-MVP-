e0f160f06352a9c937547212f41ee5da1cd60393
Melisha <mslafaber1@gmail.com>
2020-05-10 15:35:05 +0930

    vreated th emain website navigation bar whish responsively changes for mobile devices using jquery, html and added css styles with media queries to the main nav bar.

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,31 @@
-$(document).ready(function () {   
+$(document).ready(function () {
     $("#pasta, #sides, #drinks, #meals, #payment").css("display", "none");
-    $("#pizza_button").click(function () { 
+    $("#pizza_button").click(function () {
         $("#pizza").show();
         $("#pasta, #sides, #drinks, #meals, #payment").hide();
         $("#pasta_button, #sides_button, #drinks_button, #meals_button").css({"border": "none", "background-color":"#bf0603"});
         $("#pizza_button").css({"background-color":"red"});
 
     });
-    $("#pasta_button").click(function () { 
+    $("#pasta_button").click(function () {
         $("#pasta").show();
         $("#pizza, #sides, #drinks, #meals, #payment").hide();
         $("#pizza_button, #sides_button, #drinks_button, #meals_button").css({"border": "none", "background-color":"#bf0603"});
         $("#pasta_button").css({"background-color":"red"});
     });
-    $("#sides_button").click(function () { 
+    $("#sides_button").click(function () {
         $("#sides").show();
         $("#pizza, #pasta, #drinks, #meals, #payment").hide();
         $("#pasta_button, #pizza_button, #drinks_button, #meals_button").css({"border": "none", "background-color":"#bf0603"});
         $("#sides_button").css({"background-color":"red"});
     });
-    $("#drinks_button").click(function () { 
+    $("#drinks_button").click(function () {
         $("#drinks").show();
         $("#pizza, #sides, #pasta, #meals, #payment").hide();
         $("#pasta_button, #sides_button, #pizza_button, #meals_button").css({"border": "none", "background-color":"#bf0603"});
         $("#drinks_button").css({"background-color":"red"});
     });
-    $("#meals_button").click(function () { 
+    $("#meals_button").click(function () {
         $("#meals").show();
         $("#pizza, #sides, #drinks, #pasta, #payment").hide();
         $("#pasta_button, #sides_button, #drinks_button, #pizza_button").css({"border": "none", "background-color":"#bf0603"});
@@ -33,14 +33,14 @@ $(document).ready(function () {
     });
     $("#order_next_button").click(function() {
         $(".order_time").hide();
-        $(".payment_time").show(); 
+        $(".payment_time").show();
         $(".order_container").slideUp(600, function () {
             $("#payment").slideDown(300);
         });
     });
-    $("#payment_back_button").click(function() { 
+    $("#payment_back_button").click(function() {
         $(".order_time").show();
-        $(".payment_time").hide(); 
+        $(".payment_time").hide();
         $("#payment").slideUp(300, function(){
             $(".order_container").slideDown(300, function(){
                 $("#cart_contents").hide();
@@ -53,19 +53,19 @@ $(document).ready(function () {
     });
     $("#payment_next_button").click(function() {
         $(".payment_time").hide();
-        $(".confirmation_time").show(); 
+        $(".confirmation_time").show();
         $("#payment").slideUp(300, function () {
             $("#confirmation").slideDown(300);
         });
     });
     $("#confirmation_back_button").click(function() {
         $(".payment_time").show();
-        $(".confirmation_time").hide(); 
+        $(".confirmation_time").hide();
         $("#confirmation").slideUp(300, function () {
             $("#payment").slideDown(300);
         });
     });
-    $("#credit_card").click(function () { 
+    $("#credit_card").click(function () {
         $("#credit_card_form").slideDown(300, function () {
             $(".cc_elements").attr('required', true);
         });
@@ -81,7 +81,12 @@ $(document).ready(function () {
             $(".cc_elements").attr('required', false);
         });
     });
-    $("#view_cart").click(function () { 
+    $("#view_cart").click(function () {
         $("#cart_contents").slideToggle();
     });
+    //jquery for the main site navigation
+    $(".navigation-toggle").click(function() {
+      $(".navigation-toggle").toggleClass("active")
+      $("nav.sitenavigation").toggleClass("active")
+    })
 });
